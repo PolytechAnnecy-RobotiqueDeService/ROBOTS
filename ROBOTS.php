e683b91516a7109b5b8310e9e7470416f048cd01
Nicolas <nicolas.roy.8@ens.etsmtl.ca>
2022-05-10 00:19:47 +0200

    Clean-up indentation et réparation des liens vers les pages de contenu

--- ROBOTS.php
+++ ROBOTS.php
@@ -5,13 +5,13 @@
   <head>
 	<title>APP Robots</title>
 	<meta charset="UTF-8">
-    <meta content="info">
     <link rel="stylesheet" media="screen" href="lespages/ROBOTS.css" /> 
 	<link rel="icon" type="image/png" sizes="16x16" href="documents/icone_site.png"> 
   </head>
 	
   
 	<?php
+		// Définition de variables 
 		if(!isset($projectDir)){
 			$projectDir = dirname ( __FILE__ , 1 );	
 		}
@@ -24,53 +24,58 @@
 			}			
 		}
 	?>
+	
+	<?php
+		// Mécanisme de pages
+		$encours = array(" "," "," "," "," ");
+
+		if( !isset($_GET["page"]) ) { 
+			$page=0;
+		}else{
+			$page=$_GET["page"];
+		}
+		$encours[$page] = "encours";
+	?>
 
 	<body>
-	<div id="fond">
-    
-	<div id="titre">
-		<span>APP ROBOTS</span>
-	 </div>
+		<div id="fond">
+		
+			<div id="titre">
+				<span>APP ROBOTS</span>
+			 </div>
 
-	<div id="menu">
-		<ul id="lemenu">
-		<?php  
-			$encours = array(" "," "," "," "," ");
+			<div id="menu">
+				<ul id="lemenu">
+				
+				<?php
+					//TODO: Changer les noms de pages!
+					echo "<li><a href=\"?page=0\" class=\"btn_menu $encours[0]\">Accueil</a></li>\n";
+					echo "<li><a href=\"?page=1\" class=\"btn_menu $encours[1]\">Consultation</a></li>\n";
+					echo "<li><a href=\"?page=2\" class=\"btn_menu $encours[2]\">Capteurs</a></li> \n";   
+					echo "<li><a href=\"?page=3\" class=\"btn_menu $encours[3]\">Actionneurs</a></li> \n";   
+					echo "<li><a href=\"?page=4\" class=\"btn_menu $encours[4]\">Mesures</a></li> \n"; 
+				?> 
 
-			if( !isset($_GET["page"]) ) { 
-				$page=0;
-			}else{
-				$page=$_GET["page"];
-			}
-			$encours[$page] = "encours";
+				</ul>
+			</div>
+
+			<div id="contenu">
+
+				<?php
+					if( file_exists("{$projectDir}/lespages/page_".$page.".php") ){ 
+						include("{$projectDir}/lespages/page_".$page.".php");
+					}
+				?>
 			
-			echo "<li><a href=\"?page=0\" class=\"btn_menu $encours[0]\">Accueil</a></li>\n";
-			echo "<li><a href=\"?page=1\" class=\"btn_menu $encours[1]\">Consultation</a></li>\n";
-			echo "<li><a href=\"?page=2\" class=\"btn_menu $encours[2]\">Capteurs</a></li> \n";   
-			echo "<li><a href=\"?page=3\" class=\"btn_menu $encours[3]\">Actionneurs</a></li> \n";   
-			echo "<li><a href=\"?page=4\" class=\"btn_menu $encours[4]\">Mesures</a></li> \n"; 
-		?> 
+			</div>
 
-		</ul>
-	</div>
-
-	<div id="contenu">
-
-		<?php
-			if( file_exists("page_".$page.".php") ){ 
-				include("page_".$page.".php");
-			}
-		?>
-	
-	</div>
-
-	
-
-	</div>
+		</div>
+					
 		</br></br>
-	<div id="pied">
+		
+		<div id="pied">
 			<span>Polytech Annecy-Chambéry - Apprentissage Par Projets - Robots</span>
-	</div>
+		</div>
 
 	</body>
 </html>  
