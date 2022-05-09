89187db3440e5f651f8b6e533466860808f4beca
Nicolas <nicolas.roy.8@ens.etsmtl.ca>
2022-05-09 14:30:28 +0200

    Définition de variables de BDD dans bd_app.php

---
+++ lespages/bd_app.php
@@ -0,0 +1,7 @@
+<?PHP
+$mysqlUsername 	= 'sangouam';
+$mysqlPassword 	= 'gi3y7fph';
+$mysqlDatabase 	= 'sangouam';
+$mysqlHost 		= 'localhost';
+$mysqlSocket	= '/var/run/mysql/mysql5.5_epu.sock';
+?>
