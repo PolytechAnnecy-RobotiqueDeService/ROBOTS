e8eb63ad2ef8e89be799d3dd5ae0a41be17d6f7c
Nicolas <nicolas.roy.8@ens.etsmtl.ca>
2022-05-09 16:25:28 +0200

    Initial commit on dev_structure branch

--- ROBOTS.php
+++ ROBOTS.php
@@ -8,13 +8,37 @@
   </head>
   
 	<?php
-		  if( file_exists("{$projectDir}/lespages/bd_connection.php") ){ 
-			include("{$projectDir}/lespages/bd_connection.php");
-		  }
+		if(!isset($projectDir)){
+			$projectDir = dirname ( __FILE__ , 1 );	
+		}
+		if(!isset($basehttp)){
+			$login = file_get_contents("{$projectDir}/VOTRE_LOGIN.txt");
+			$basehttp = "https://tp-epua.univ-smb.fr/~{$login}/";	
+		}	
 	?>
+	
+	<?php  
+		$encours = array(" "," "," "," "," ");
+
+		if( !isset($_GET["page"]) ) { 
+			$page=0;
+		}else{
+			$page=$_GET["page"];
+		}
+		$encours[$page] = "encours";
+	?> 
+	
 
 	<body>
-	
+		<div id="main">
+			<?php
+				if( file_exists("{$projectDir}/lespages/page_".$page.".php") ){ 
+					include("{$projectDir}/lespages/page_".$page.".php");
+				}else{
+					echo("Fichier {$projectDir}/lespages/page_".$page.".php est inexistant.");
+				}
+			?>
+		</div>
 	</body>
 </html>  
   
