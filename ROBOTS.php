33e0c1389391e8408a77cbc1e38115fa25af6f7c
Cloé Léglise <cloe.leglise@free.fr>
2022-05-09 16:40:22 +0200

    Ajout structure
    Ajout du premier essai des pages de structure ROBOTS.php et ROBOTS.css

--- ROBOTS.php
+++ ROBOTS.php
@@ -3,42 +3,68 @@
 <html>
 
   <head>
-    <title></title>
-    <meta charset="UTF-8">
+	<title>APP Robots</title>
+	<meta charset="UTF-8">
+    <meta content="info">
+    <link rel="stylesheet" media="screen" href="lespages/ROBOTS.css" />  
   </head>
   
 	<?php
-		if(!isset($projectDir)){
+		/*if(!isset($projectDir)){
 			$projectDir = dirname ( __FILE__ , 1 );	
 		}
 		if(!isset($basehttp)){
 			$login = file_get_contents("{$projectDir}/VOTRE_LOGIN.txt");
 			$basehttp = "https://tp-epua.univ-smb.fr/~{$login}/";	
-		}	
+		}	*/
 	?>
 	
-	<?php  
-		$encours = array(" "," "," "," "," ");
-
-		if( !isset($_GET["page"]) ) { 
-			$page=0;
-		}else{
-			$page=$_GET["page"];
-		}
-		$encours[$page] = "encours";
-	?> 
 	
 
 	<body>
-		<div id="main">
-			<?php
-				if( file_exists("{$projectDir}/lespages/page_".$page.".php") ){ 
-					include("{$projectDir}/lespages/page_".$page.".php");
-				}else{
-					echo("Fichier {$projectDir}/lespages/page_".$page.".php est inexistant.");
-				}
-			?>
-		</div>
+	<div id="fond">
+    
+	<div id="titre">
+		<span>APP ROBOTS</span>
+	 </div>
+
+	<div id="menu">
+		<ul id="lemenu">
+		<?php  
+			$encours = array(" "," "," "," "," ");
+
+			if( !isset($_GET["page"]) ) { 
+				$page=0;
+			}else{
+				$page=$_GET["page"];
+			}
+			$encours[$page] = "encours";
+			
+			echo "<li><a href=\"?page=0\" class=\"btn_menu $encours[0]\">Accueil</a></li>\n";
+			echo "<li><a href=\"?page=1\" class=\"btn_menu $encours[1]\">Consultation</a></li>\n";
+			echo "<li><a href=\"?page=2\" class=\"btn_menu $encours[2]\">Capteurs</a></li> \n";   
+			echo "<li><a href=\"?page=3\" class=\"btn_menu $encours[3]\">Actionneurs</a></li> \n";   
+			echo "<li><a href=\"?page=4\" class=\"btn_menu $encours[4]\">Mesures</a></li> \n"; 
+		?> 
+
+		</ul>
+	</div>
+
+	<div id="contenu">
+
+		<?php
+			if( file_exists("page_".$page.".php") ){ 
+				include("page_".$page.".php");
+			}
+		?>
+	
+	</div>
+
+	<div id="pied">
+		<span>Polytech Annecy-Chambéry - Apprentissage Par Projets - Robots</span>
+	</div>
+
+</div>
 	</body>
 </html>  
   
