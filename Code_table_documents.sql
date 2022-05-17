d46f63c9cae18efaf72745d98f0c0c290b8bc379
KellTalent <90306635+KellTalent@users.noreply.github.com>
2022-05-17 08:54:13 +0200

    Add files via upload

--- Code_table_documents.sql
+++ Code_table_documents.sql
@@ -12,10 +12,10 @@ CREATE TABLE Documents
 
 INSERT INTO Documents (id_doc, titre, date_rendu, type_doc, chemin)
  VALUES 
- (1, 'APP_Robot_Seance_1', '09/02/22', 'CR', 'chemin'),
- (2, 'APP_Robot_Seance_2', '02/03/22', 'CR', 'chemin'),
- (3, 'APP_Robot_Seance_3', '16/03/22', 'CR', 'chemin'),
- (4, 'APP_Robot_Seance_4', '29/03/22', 'CR', 'chemin'),
- (5, 'APP_Robot_Seance_5', '13/04/22', 'CR', 'chemin'),
- (6, 'APP_Robot_Seance_6', '26/04/22', 'CR', 'chemin'),
- (7, 'APP_Robot_Seance_7', '04/05/22', 'CR', 'chemin');
+ (1, 'APP_Robot_Seance_1', '09/02/22', 'CR', 'E:\public_html\ROBOTS\Documents'),
+ (2, 'APP_Robot_Seance_2', '02/03/22', 'CR', 'E:\public_html\ROBOTS\Documents'),
+ (3, 'APP_Robot_Seance_3', '16/03/22', 'CR', 'E:\public_html\ROBOTS\Documents'),
+ (4, 'APP_Robot_Seance_4', '29/03/22', 'CR', 'E:\public_html\ROBOTS\Documents'),
+ (5, 'APP_Robot_Seance_5', '13/04/22', 'CR', 'E:\public_html\ROBOTS\Documents'),
+ (6, 'APP_Robot_Seance_6', '26/04/22', 'CR', 'E:\public_html\ROBOTS\Documents'),
+ (7, 'APP_Robot_Seance_7', '04/05/22', 'CR', 'E:\public_html\ROBOTS\Documents');
