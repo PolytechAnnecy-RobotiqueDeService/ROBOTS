da934a7b9dd669b6f4465ac1a2c36edf5a95e55b
Nicolas <nicolas.roy.8@ens.etsmtl.ca>
2022-05-22 17:29:00 +0200

    Renaming titles

--- lespages/page_3.php
+++ lespages/page_3.php
@@ -10,7 +10,7 @@
 		<div id="Team">
 			<h1>L'équipe</h1>
 			<p> L'apprentissage par projets "APP" est un projet qui accompagne les élèves de la filière SNI tout au long de leur formation, ce projet est basé sur un travail d'équipe dans un contexte de mise en situation professionnelle où l'élève est acteur de sa formation. <br>La "Robotique de service" est un thème important présent parmi les APP, qui permet aux étudiants de gérer un projet et d'approfondir leur savoir dans plusieurs domaines. <br> <br> <br>La photo ci-dessous réunit Les membres de la promotion 2024 de l'équipe de l'APP du thème "Robotique de service": </p>
-			<img src="documents/images/Team.jpg" alt="id1" width="700" height="500">
+			<img src="documents/images/Team.jpg" alt="id1" width="700">
 			<br> <br> <br>
 		</div>
 
@@ -28,7 +28,7 @@
 
 			<p> Pour couvrir les différentes axes de notre projet, Notre Equipe est composée de 8 membres de différentes formations, répartissant les différentes tâches tout au long du projet pour pouvoir effectuer la globalité des tâches nécessaires dans notre projet.<br> L'organisation de notre équipe est composée comme suit: </p>
 			
-			<h2>Chef du projet</h2>
+			<h2>Chef de projet</h2>
 			<div class="team">
 				<div class="membre">
 					<img src="documents/images/Equipe_Florian_crop.jpg" alt="Avatar">
@@ -38,7 +38,7 @@
 				</div>
 			</div>
 			
-			<h2> L'équipe de Gestion du robot Pepper:</h2>
+			<h2> L'équipe responsable du développement du robot Pepper:</h2>
 			<div class="team">
 				<div class="membre">
 					<img src="documents/images/Equipe_Nicolas_crop.jpg">
@@ -60,7 +60,7 @@
 				</div>
 			</div>
 			
-			<h2> Responsable de la gestion du Robot Nao: </h2>
+			<h2> Responsable du développement du robot Nao: </h2>
 			<div class="team">
 				<div class="membre">
 					<img src="documents/images/Equipe_Jules_crop.jpg">
@@ -80,7 +80,7 @@
 				</div>
 			</div>
 			
-			<h2> Responsable de la gestion du Serveur: </h2>
+			<h2> Responsable de l'implémentation du serveur: </h2>
 			<div class="team">
 				<div class="membre">
 					<img src="documents/images/Equipe_Mehdi_crop.jpg">
@@ -90,7 +90,7 @@
 				</div>
 			</div>
 			
-			<h2> Responsable de la gestion de la tablette de Pepper: </h2>
+			<h2> Responsable de l'interface utilisateur de Pepper: </h2>
 			<div class="team">
 				<div class="membre">
 					<img src="documents/images/Equipe_Hamza_crop.jpg">
