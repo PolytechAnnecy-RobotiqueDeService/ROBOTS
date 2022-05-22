bf347140818276492043014a0c74d19b19f1a52a
Nicolas <nicolas.roy.8@ens.etsmtl.ca>
2022-05-22 19:45:46 +0200

    Ajout des fichiers de Camille

---
+++ lespages/page_2.php
@@ -0,0 +1,46 @@
+<html>
+<link rel="stylesheet" href="./lespages/page_2.css"/>
+<head><title>Suivi de projet</title></head>
+<link rel="icon" type="image/png" sizes="16x16" href="pepper.png">
+
+<body>
+    <div id = "titre">
+    <h1>Suivi du projet :</h1>
+    </div>
+    <div id = "en_cours">
+        <p>Bienvenue sur la page de suivi de notre projet. Ici, vous retrouverez un résumé de toutes nos avancées techniques, ainsi que des informations sur les aspects en cours de développement !</p>
+        <!--<p id=video1><video controls width="500" height="320"><source src="./documents/videos/video1.mp4"></video></p></br>
+        potentiellement une video de presentation de pepper
+        -->
+        <p><strong>Développement en cours :</strong></p>
+        <ul>
+            <li>Essayer de faire se déplacer Pepper par suivi de mur à l'aide de ses capteurs de distance. Voici une vidéo de démonstration où Pepper négocie un virage :</br>
+            <p id=video1><video controls width="500" height="320"><source src="./documents/videos/video1.mp4"></video></p></br>
+            </li>
+            <li>Alternance entre le suivis de mur gauche ou droit</li>
+            <li>Développement d'une méthode de synthèse vocale pour NAO ou Pepper dans le but de faciliter les interactions orales</li>
+            <li>Développement du réseau pour la communication entre les robots et un serveur</li>
+            <li>Création de la page web qui sera affichée sur Pepper, qui permettrait notamment de lui demander un guidage particulier</li>
+        </ul>
+        </br>
+    </div>
+    <div id="update1">
+        <p><strong>Développement terminé :</strong></p>
+        <ul>
+            <li>Entraînement de Nao à reconnaître un texte dicté mot à mot grâce à une banque de mots reconnus contenus dans un fichier texte</li>
+            <li>Suivi d'un mur par Pepper et correction de sa trajectoire en ligne droite</li>
+    </div>
+
+    <!--<div id="galerie_photo">    il faudrait rajouter des photos dans la galerie
+        </br>
+        <p>Galerie photo :</p>
+        <div id = "image"><img src = "./documents/images/image1.jpg" sizes="16x16"><img src = "./documents/images/image2.jpg" sizes="16x16"><img src = "./documents/images/image1.jpg" sizes="16x16"><img src = "./documents/images/image1.jpg" sizes="16x16"></div>
+    </div>
+        <img src = "pepper2.jpg">
+    <div>
+    
+
+    </div>
+    -->
+</body>
+</html>
