53a915e25237418682b16364150bab56db74f740
Nicolas <nicolas.roy.8@ens.etsmtl.ca>
2022-05-22 19:54:28 +0200

    Corrections pour l'intégration

--- lespages/page_2.php
+++ lespages/page_2.php
@@ -4,9 +4,8 @@
 <link rel="icon" type="image/png" sizes="16x16" href="pepper.png">
 
 <body>
-    <div id = "titre">
     <h1>Suivi du projet :</h1>
-    </div>
+    
     <div id = "en_cours">
         <p>Bienvenue sur la page de suivi de notre projet. Ici, vous retrouverez un résumé de toutes nos avancées techniques, ainsi que des informations sur les aspects en cours de développement !</p>
         <!--<p id=video1><video controls width="500" height="320"><source src="./documents/videos/video1.mp4"></video></p></br>
@@ -15,11 +14,15 @@
         <p><strong>Développement en cours :</strong></p>
         <ul>
             <li>Essayer de faire se déplacer Pepper par suivi de mur à l'aide de ses capteurs de distance. Voici une vidéo de démonstration où Pepper négocie un virage :</br>
-            <p id=video1><video controls width="500" height="320"><source src="./documents/videos/video1.mp4"></video></p></br>
+            <p id=video1><video controls width="500" height="320"><source src="./documents/videos/capsule_demo_deplacement.mp4"></video></p>
             </li>
+			</br>
             <li>Alternance entre le suivis de mur gauche ou droit</li>
+			</br>
             <li>Développement d'une méthode de synthèse vocale pour NAO ou Pepper dans le but de faciliter les interactions orales</li>
+			</br>
             <li>Développement du réseau pour la communication entre les robots et un serveur</li>
+			</br>
             <li>Création de la page web qui sera affichée sur Pepper, qui permettrait notamment de lui demander un guidage particulier</li>
         </ul>
         </br>
@@ -28,6 +31,7 @@
         <p><strong>Développement terminé :</strong></p>
         <ul>
             <li>Entraînement de Nao à reconnaître un texte dicté mot à mot grâce à une banque de mots reconnus contenus dans un fichier texte</li>
+			</br>
             <li>Suivi d'un mur par Pepper et correction de sa trajectoire en ligne droite</li>
     </div>
 
